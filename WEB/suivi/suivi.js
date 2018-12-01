d122e3bacdeb8fd9b0dfd329639937cca9e6a0d2
MorganArthox <morgan.chetelat@outlook.com>
2018-12-01 16:45:00 +0100

    Début de la page de suivi

---
+++ WEB/suivi/suivi.js
@@ -0,0 +1,7 @@
+function suiviCommande (){
+  document.getElementById('comAttente').textContent = '5';
+  document.getElementById('comPreparation').textContent = '5';
+  document.getElementById('comRecuperer').textContent = '5';
+};
+
+suiviCommande();
