433a19ccb15414b015105b0ef15b16bb081e5bc8
MorganArthox <morgan.chetelat@outlook.com>
2018-12-08 18:44:58 +0100

    changement du texte sur la page du server

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Bonsoir\n');
+  res.end('Ceci est la page du server ;)\n');
 });
 const io = require('socket.io')(server);
 
